439c6f1416adc293937c25ad6689c79668713395
clach04 <clach04@gmail.com>
2025-04-10 20:27:51 -0700

    Include version in config page

--- src/pkjs/config.js
+++ src/pkjs/config.js
@@ -1,3 +1,5 @@
+var meta = require('../../package.json');
+
 module.exports = [
     { 
         "type": "heading", 
@@ -215,5 +217,9 @@ module.exports = [
     {
         "type": "submit",
         "defaultValue": "Save"
+    },
+    {
+        "type": "text",
+        "defaultValue": "v" + meta.version
     }
 ];
