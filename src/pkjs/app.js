245dc63240ba82f40f8acfc1ad43edeb95d2e64a
clach04 <clach04@gmail.com>
2025-04-13 15:16:37 -0700

    Additional confg debug logging

--- src/pkjs/app.js
+++ src/pkjs/app.js
@@ -13,8 +13,28 @@ Pebble.addEventListener('ready', function(e) {
         },
         function(e) {
             console.log('failed to send options to pebble. Error: ' + e.error.message);
+            console.log(JSON.stringify(e));
         }
     );
     // TODO is it possible to lookup defaults and send them now?
 
+});
+
+// override send config, in case want to modify (change types) values or debug
+Pebble.addEventListener('webviewclosed', function(e) {
+    if (e && !e.response) {
+        return;
+    }
+
+    var dict = clay.getSettings(e.response);
+    //console.log(dict.VIBRATE_ON_DISCONNECT);
+
+    console.log('string config data length=' + JSON.stringify(e).length);
+    // Send settings values to watch side
+    Pebble.sendAppMessage(dict, function(e) {
+        console.log('Sent config data to Pebble');
+    }, function(e) {
+        console.log('Failed to send data options to Pebble. Error: ' + e.error.message);
+        console.log(JSON.stringify(e));  // NOTE possibly secret leakage if adding secrets to config
+    });
 });
